051d6a4a3b55bd7c956bc779c5f95cc6aa064e2a
Ronan Barzic <rbarzic@gmail.com>
2015-11-24 00:02:35 +0100

    Compile OK but does not work

--- src/main.c
+++ src/main.c
@@ -13,10 +13,11 @@
 #include "nrf_delay.h"
 
 #include "nrf_clock.h"
-#include "nrf_rtc.h"
+
 #include "nrf_gpio.h"
 
-#include "nrf_saadc.h"
+#include "nrf_pwm.h"
+
 
 // Autocompletion of nrf specific symbols and eldoc work much better when those
 // files are included on top level
@@ -34,30 +35,44 @@
 
 #include "uart.h"
 
-#define PIN_LED 19 // LED1
-#define ADC_RESULT_BUFFER_SIZE 1
+#define PIN_LED3 19
+#define PIN_LED4 20
 
-static volatile unsigned irq_counter = 0;
-static volatile unsigned irq_triggered = 0;
+void dbg_pwm_registers(NRF_PWM_Type * NRF_PWMx) {
+    printf("ENABLE            = 0x%08x\n",(unsigned)NRF_PWMx->ENABLE);
+    printf("MODE              = 0x%08x\n",(unsigned)NRF_PWMx->MODE);
+    printf("COUNTERTOP        = 0x%08x\n",(unsigned)NRF_PWMx->COUNTERTOP);
+    printf("PRESCALER         = 0x%08x\n",(unsigned)NRF_PWMx->PRESCALER);
+    printf("DECODER           = 0x%08x\n",(unsigned)NRF_PWMx->DECODER);
+    printf("LOOP              = 0x%08x\n",(unsigned)NRF_PWMx->LOOP);
+    printf("SEQ[0].PTR        = 0x%08x\n",(unsigned)NRF_PWMx->SEQ[0].PTR);
+    printf("SEQ[0].CNT        = 0x%08x\n",(unsigned)NRF_PWMx->SEQ[0].CNT);
+    printf("SEQ[0].REFRESH    = 0x%08x\n",(unsigned)NRF_PWMx->SEQ[0].REFRESH);
+    printf("SEQ[0].ENDDELAY   = 0x%08x\n",(unsigned)NRF_PWMx->SEQ[0].ENDDELAY);
+    printf("SEQ[1].PTR        = 0x%08x\n",(unsigned)NRF_PWMx->SEQ[1].PTR);
+    printf("SEQ[1].CNT        = 0x%08x\n",(unsigned)NRF_PWMx->SEQ[1].CNT);
+    printf("SEQ[1].REFRESH    = 0x%08x\n",(unsigned)NRF_PWMx->SEQ[1].REFRESH);
+    printf("SEQ[1].ENDDELAY   = 0x%08x\n",(unsigned)NRF_PWMx->SEQ[1].ENDDELAY);
+    printf("PSEL.OUT[0]       = 0x%08x\n",(unsigned)NRF_PWMx->PSEL.OUT[0]);
+    printf("PSEL.OUT[1]       = 0x%08x\n",(unsigned)NRF_PWMx->PSEL.OUT[1]);
+    printf("PSEL.OUT[2]       = 0x%08x\n",(unsigned)NRF_PWMx->PSEL.OUT[2]);
+    printf("PSEL.OUT[3]       = 0x%08x\n",(unsigned)NRF_PWMx->PSEL.OUT[3]);
+    printf("EVENTS_SEQSTARTED[0]       = 0x%08x\n",(unsigned)NRF_PWMx->EVENTS_SEQSTARTED[0]);
+    printf("EVENTS_SEQSTARTED[1]       = 0x%08x\n",(unsigned)NRF_PWMx->EVENTS_SEQSTARTED[1]);
+    printf("EVENTS_STOPPED             = 0x%08x\n",(unsigned)NRF_PWMx->EVENTS_STOPPED);
+    printf("EVENTS_PWMPERIODEND        = 0x%08x\n",(unsigned)NRF_PWMx->EVENTS_PWMPERIODEND);
 
-#define NRF_SAADC_EVENT_DONE offsetof(NRF_SAADC_Type, EVENTS_DONE)
-
-#define  RTC_PRESCALER_VAL 1
-
-void RTC0_IRQHandler(void) {
-    irq_counter++;
-    irq_triggered=1;
-    nrf_rtc_event_clear(NRF_RTC0,NRF_RTC_EVENT_COMPARE_0);
-    nrf_rtc_task_trigger(NRF_RTC0,NRF_RTC_TASK_CLEAR);
-    nrf_gpio_pin_toggle(PIN_LED);
 }
 
 int main(void)
 {
 
-    nrf_saadc_value_t result;;
-    nrf_gpio_cfg_output(PIN_LED);
-    nrf_gpio_pin_set(PIN_LED); // LED is off
+    nrf_gpio_cfg_output(PIN_LED3);
+    nrf_gpio_pin_set(PIN_LED3); // LED is off
+
+    nrf_gpio_cfg_output(PIN_LED4);
+    nrf_gpio_pin_set(PIN_LED4); // LED is off
+
 
     uint32_t err_code;
     const app_uart_comm_params_t comm_params =
@@ -81,102 +96,60 @@ int main(void)
     APP_ERROR_CHECK(err_code);
 
 
-    printf("Hello world ! (%s)\n",__DATE__);
+    printf("Hello world !! (%s)\n",__DATE__);
 
 
 
 
 
-
-    // Irq setup
-    NVIC_SetPriority(RTC0_IRQn, 15); // Lowes priority
-    NVIC_ClearPendingIRQ(RTC0_IRQn);
-    NVIC_EnableIRQ(RTC0_IRQn);
-
-    // Start LFCLK clock
-    nrf_clock_lf_src_set(NRF_CLOCK_LF_SRC_RC); // 32KHz RC
-    nrf_clock_task_trigger(NRF_CLOCK_TASK_LFCLKSTART);
-
-
-    // One hertz RTC event generation
-
-    nrf_rtc_prescaler_set(NRF_RTC0,RTC_PRESCALER_VAL);
-    nrf_rtc_cc_set(NRF_RTC0,0 /*ch*/, (32768/(1+RTC_PRESCALER_VAL)));
-
-
-    nrf_rtc_event_enable(NRF_RTC0, NRF_RTC_EVENT_COMPARE_0);
-    nrf_rtc_int_enable(NRF_RTC0,NRF_RTC_INT_COMPARE0_MASK);
-    nrf_rtc_task_trigger(NRF_RTC0,NRF_RTC_TASK_START);
-
-
-
-
-    nrf_saadc_channel_config_t config;
-    config.acq_time = NRF_SAADC_ACQTIME_20US;
-    config.gain = NRF_SAADC_GAIN1_4;
-    config.mode = NRF_SAADC_MODE_SINGLE_ENDED;
-    config.pin_p = NRF_SAADC_INPUT_AIN0;
-    config.pin_n = NRF_SAADC_INPUT_AIN0; // Single ended -> should be ground to zero
-    config.reference = NRF_SAADC_REFERENCE_VDD4;
-    config.resistor_p = NRF_SAADC_RESISTOR_DISABLED;
-    config.resistor_n = NRF_SAADC_RESISTOR_DISABLED;
-
-    nrf_saadc_enable();
-    nrf_saadc_channel_init(NRF_SAADC_INPUT_AIN0, &config);
-    nrf_saadc_buffer_init(&result,1); // One sample
-
-    //for (i = 0; i < 1000; i++) {
-    //    result = 0;
-    //    nrf_saadc_task_trigger(NRF_SAADC_TASK_START);
-    //
-    //    while(!nrf_saadc_event_check(NRF_SAADC_EVENT_STARTED));
-    //    nrf_saadc_event_clear(NRF_SAADC_EVENT_STARTED);
-    //
-    //    nrf_saadc_task_trigger(NRF_SAADC_TASK_SAMPLE);
-    //
-    //
-    //    while(!nrf_saadc_event_check(NRF_SAADC_EVENT_END));
-    //    nrf_saadc_event_clear(NRF_SAADC_EVENT_END);
-    //
-    //    nrf_saadc_task_trigger(NRF_SAADC_TASK_STOP);
-    //    while(!nrf_saadc_event_check(NRF_SAADC_EVENT_STOPPED));
-    //    nrf_saadc_event_clear(NRF_SAADC_EVENT_STOPPED);
-    //
-    //    printf("ADC result = 0x%08x\n",result);
-    //    nrf_delay_ms(500);
-    //
-    //}
-
-    uint32_t local_counter = 0;
-    uint32_t iter = 0;
-    while(1) {
-
-        if(irq_triggered==1) { // The UART is also generating
-                               // interrupt so we don't want extra
-                               // conversion
-            irq_triggered=0;
-            iter++;
-            result = 0;
-            nrf_saadc_task_trigger(NRF_SAADC_TASK_START);
-
-            while(!nrf_saadc_event_check(NRF_SAADC_EVENT_STARTED));
-            nrf_saadc_event_clear(NRF_SAADC_EVENT_STARTED);
-
-            nrf_saadc_task_trigger(NRF_SAADC_TASK_SAMPLE);
+#define PWM_COUNTER_TOP ((uint16_t)16000) // 1ms
+#define PWM_CH0_DUTY ((uint16_t)8000) // 50%
+#define PWM_CH1_DUTY ((uint16_t)16000) // 100%
+    if(0) { // From datasheet
+        uint16_t pwm_seq[2] = {PWM_CH0_DUTY, PWM_CH1_DUTY};
+NRF_PWM0->PSEL.OUT[0] = ( PIN_LED3 << PWM_PSEL_OUT_PIN_Pos) |
+                        (PWM_PSEL_OUT_CONNECT_Connected <<
+                                                 PWM_PSEL_OUT_CONNECT_Pos);
+NRF_PWM0->PSEL.OUT[1] = (PIN_LED4  << PWM_PSEL_OUT_PIN_Pos) |
+                        (PWM_PSEL_OUT_CONNECT_Connected <<
+                                                 PWM_PSEL_OUT_CONNECT_Pos);
+NRF_PWM0->ENABLE      = (PWM_ENABLE_ENABLE_Enabled << PWM_ENABLE_ENABLE_Pos);
+NRF_PWM0->MODE        = (PWM_MODE_UPDOWN_Up << PWM_MODE_UPDOWN_Pos);
+NRF_PWM0->PRESCALER   = (PWM_PRESCALER_PRESCALER_DIV_1 <<
+                                                 PWM_PRESCALER_PRESCALER_Pos);
+NRF_PWM0->COUNTERTOP  = (16000 << PWM_COUNTERTOP_COUNTERTOP_Pos); //1 msec
+NRF_PWM0->LOOP        = (PWM_LOOP_CNT_Disabled << PWM_LOOP_CNT_Pos);
+NRF_PWM0->DECODER   = (PWM_DECODER_LOAD_Individual << PWM_DECODER_LOAD_Pos) |
+                      (PWM_DECODER_MODE_RefreshCount << PWM_DECODER_MODE_Pos);
+NRF_PWM0->SEQ[0].PTR  = ((uint32_t)(pwm_seq) << PWM_SEQ_PTR_PTR_Pos);
+NRF_PWM0->SEQ[0].CNT  = ((sizeof(pwm_seq) / sizeof(uint16_t)) <<
+                                                 PWM_SEQ_CNT_CNT_Pos);
+NRF_PWM0->SEQ[0].REFRESH  = 0;
+NRF_PWM0->SEQ[0].ENDDELAY = 0;
+NRF_PWM0->TASKS_SEQSTART[0] = 1;
 
 
-            while(!nrf_saadc_event_check(NRF_SAADC_EVENT_END));
-            nrf_saadc_event_clear(NRF_SAADC_EVENT_END);
 
-            nrf_saadc_task_trigger(NRF_SAADC_TASK_STOP);
-            while(!nrf_saadc_event_check(NRF_SAADC_EVENT_STOPPED));
-            nrf_saadc_event_clear(NRF_SAADC_EVENT_STOPPED);
+    }
 
-            printf("%08lu ADC result = 0x%08x (irq=%d)\n",iter,result,irq_counter);
+    if (1){
+        uint16_t pwm_seq[2] = {PWM_CH0_DUTY, PWM_CH1_DUTY};
 
+        nrf_pwm_pselout_set(NRF_PWM0, NRF_PWM_CHANNEL0, PIN_LED3);
+        nrf_pwm_pselout_set(NRF_PWM0, NRF_PWM_CHANNEL1, PIN_LED4);
+        nrf_pwm_enable(NRF_PWM0);
+        nrf_pwm_mode_set(NRF_PWM0, NRF_PWM_MODE_UP);
+        nrf_pwm_prescaler_set(NRF_PWM0, NRF_PWM_PRESCALER_DIV_1);
+        nrf_pwm_countertop_set(NRF_PWM0, PWM_COUNTER_TOP);
+        nrf_pwm_loop_set(NRF_PWM0, 0); // disabled
+        nrf_pwm_decoder_set(NRF_PWM0,
+                            NRF_PWM_DECODER_LOAD_INDIVIDUAL,
+                            NRF_PWM_DECODER_MODE_REFRESHCOUNT);
 
-        }
-
-        __WFI();
-    };
+        nrf_pwm_seq_set(NRF_PWM0, NRF_PWM_SEQUENCE0, (uint32_t)pwm_seq,
+                        ((sizeof(pwm_seq))/sizeof(uint16_t)), 0, 0);
+        nrf_pwm_task_trigger(NRF_PWM0, NRF_PWM_TASK_SEQSTART0);
+    }
+    dbg_pwm_registers(NRF_PWM0);
+    while(1);
 }
---
+++ src/nrf_pwm.h
@@ -0,0 +1,316 @@
+// Unfortunatly, there is no pwm-related hal file in the nrf52 SDK
+// so we make our own
+
+#pragma once
+
+
+#include <stdint.h>
+
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "nrf.h"
+
+/**
+ * @enum nrf_pwm_task_t
+ * @brief Pwm tasks.
+ */
+typedef enum
+{
+    /*lint -save -e30 -esym(628,__INTADDR__)*/
+    // NRF_PWM_TASK_START    = offsetof(NRF_PWM_Type, TASKS_START),  /* Missing in the datasheet ??*/
+    NRF_PWM_TASK_STOP     = offsetof(NRF_PWM_Type, TASKS_STOP),
+    NRF_PWM_TASK_SEQSTART0 = offsetof(NRF_PWM_Type, TASKS_SEQSTART[0]),
+    NRF_PWM_TASK_SEQSTART1 = offsetof(NRF_PWM_Type, TASKS_SEQSTART[1]),
+    NRF_PWM_TASK_NEXTSTEP = offsetof(NRF_PWM_Type, TASKS_NEXTSTEP),
+    /*lint -restore*/
+} nrf_pwm_task_t;
+
+
+/**
+ * @enum nrf_pwm_event_t
+ * @brief Pwm events.
+ */
+typedef enum
+{
+    /*lint -save -e30*/
+    NRF_PWM_EVENT_STOPPED = offsetof(NRF_PWM_Type, EVENTS_STOPPED),
+    NRF_PWM_EVENT_SEQSTARTED0 = offsetof(NRF_PWM_Type, EVENTS_SEQSTARTED[0]),
+    NRF_PWM_EVENT_SEQSTARTED1 = offsetof(NRF_PWM_Type, EVENTS_SEQSTARTED[1]),
+    NRF_PWM_EVENT_SEQEND0 = offsetof(NRF_PWM_Type, EVENTS_SEQEND[0]),
+    NRF_PWM_EVENT_SEQEND1 = offsetof(NRF_PWM_Type, EVENTS_SEQEND[1]),
+    NRF_PWM_EVENT_PWNPERIODEND = offsetof(NRF_PWM_Type, EVENTS_PWMPERIODEND),
+    NRF_PWM_EVENT_LOOPSDONE = offsetof(NRF_PWM_Type, EVENTS_LOOPSDONE),
+    /*lint -restore*/
+} nrf_pwm_event_t;
+
+/**
+ * @enum nrf_pwm_channel_t
+ * @brief Pwm channels.
+ */
+typedef enum
+{
+    NRF_PWM_CHANNEL0 = 0, /**< Pwm channel 0. */
+    NRF_PWM_CHANNEL1,     /**< Pwm channel 1. */
+    NRF_PWM_CHANNEL2,     /**< Pwm channel 2. */
+    NRF_PWM_CHANNEL3      /**< Pwm channel 3. */
+} nrf_pwm_channel_t;
+
+
+/**
+ * @enum nrf_pwm_channel_t
+ * @brief Pwm channels.
+ */
+typedef enum
+{
+    NRF_PWM_PRESCALER_DIV_1=0,  // 0 Divide by 1 ( 16MHz)
+    NRF_PWM_PRESCALER_DIV_2  ,  // 1 Divide by 2 ( 8MHz)
+    NRF_PWM_PRESCALER_DIV_4,    // 2 Divide by 4 ( 4MHz)
+    NRF_PWM_PRESCALER_DIV_8,    // 3 Divide by 8 ( 2MHz)
+    NRF_PWM_PRESCALER_DIV_16,   // 4 Divide by 16 ( 1MHz)
+    NRF_PWM_PRESCALER_DIV_32,   // 5 Divide by 32 ( 500kHz)
+    NRF_PWM_PRESCALER_DIV_64,   // 6 Divide by 64 ( 250kHz)
+    NRF_PWM_PRESCALER_DIV_128,  // 7 Divide by 128 ( 125kHz)
+} nrf_pwm_prescaler_t;
+
+
+
+
+/**
+ * @enum nrf_pwm_mode_t
+ * @brief Pwm modes.
+ */
+typedef enum
+{
+    NRF_PWM_MODE_UP = 0, /**< Pwm mode 0. */
+    NRF_PWM_MODE_UPANDDOWN,     /**< Pwm mode 1. */
+} nrf_pwm_mode_t;
+
+
+/**
+ * @enum nrf_pwm_decoder_load_t
+ * @brief Pwm DECODER load field value.
+ */
+typedef enum
+{
+    NRF_PWM_DECODER_LOAD_COMMON = 0, // 0
+    NRF_PWM_DECODER_LOAD_GROUPED,    // 1
+    NRF_PWM_DECODER_LOAD_INDIVIDUAL, // 2
+    NRF_PWM_DECODER_LOAD_WAVEFORM ,  // 3
+
+} nrf_pwm_decoder_load_t;
+
+
+/**
+ * @enum nrf_pwm_decoder_mode_t
+ * @brief Pwm DECODER mode field value.
+ */
+typedef enum
+{
+    NRF_PWM_DECODER_MODE_REFRESHCOUNT = 0, // 0
+    NRF_PWM_DECODER_MODE_NEXTSTEP,        // 1
+
+} nrf_pwm_decoder_mode_t;
+
+/**
+ * @enum nrf_pwm_sequence_t
+ * @brief Pwm DECODER sequence number.
+ */
+typedef enum
+{
+    NRF_PWM_SEQUENCE0 = 0, // 0
+    NRF_PWM_SEQUENCE1,     // 1
+
+} nrf_pwm_sequence_t;
+
+
+
+
+
+// Helper functions
+
+
+/**
+ * @brief Function for activating a specific pwm task.
+ *
+ * @param NRF_PWMx Pwm instance.
+ *
+ * @param pwm_task Pwm task.
+ */
+__STATIC_INLINE void nrf_pwm_task_trigger(NRF_PWM_Type * NRF_PWMx, nrf_pwm_task_t pwm_task)
+{
+    *((volatile uint32_t *)((uint8_t *)NRF_PWMx + (uint32_t)pwm_task)) = 0x1UL;
+}
+
+
+/**
+ * @brief Function for clearing a specific pwm event.
+ *
+ * @param NRF_PWMx Pwm instance.
+ *
+ * @param pwm_event Pwm event to clear.
+ */
+__STATIC_INLINE void nrf_pwm_event_clear(NRF_PWM_Type * NRF_PWMx,
+                                           nrf_pwm_event_t pwm_event)
+{
+    *((volatile uint32_t *)((uint8_t *)NRF_PWMx + (uint32_t)pwm_event)) = 0x0UL;
+}
+
+/**
+ * @brief Function for returning the state of a specific event.
+ *
+ * @param NRF_PWMx Pwm instance.
+ *
+ * @param pwm_event Pwm event to check.
+ */
+__STATIC_INLINE bool nrf_pwm_event_check(NRF_PWM_Type * NRF_PWMx,
+                                           nrf_pwm_event_t pwm_event)
+{
+    return (bool)*(volatile uint32_t *)((uint8_t *)NRF_PWMx + (uint32_t)pwm_event);
+}
+
+
+/**
+ * @brief Function for setting pin for a specific channel
+ *
+ * @param NRF_PWMx Pwm instance.
+ *
+ * @param channel Channel number
+ *
+ * @param pin_number Pin number.
+ */
+__STATIC_INLINE void nrf_pwm_pselout_set(NRF_PWM_Type * NRF_PWMx, nrf_pwm_channel_t channel,uint32_t pin_number)
+{
+    NRF_PWMx->PSEL.OUT[channel] = (pin_number << PWM_PSEL_OUT_PIN_Pos) |
+        (PWM_PSEL_OUT_CONNECT_Connected << PWM_PSEL_OUT_CONNECT_Pos);
+}
+
+/**
+ * @brief Function to enable a PWM
+ *
+ * @param NRF_PWMx Pwm instance.
+ *
+ */
+__STATIC_INLINE void nrf_pwm_enable(NRF_PWM_Type * NRF_PWMx)
+{
+    NRF_PWMx->ENABLE = 1;
+}
+
+/**
+ * @brief Function to disable a PWM
+ *
+ * @param NRF_PWMx Pwm instance.
+ *
+ */
+__STATIC_INLINE void nrf_pwm_disable(NRF_PWM_Type * NRF_PWMx)
+{
+    NRF_PWMx->ENABLE = 0;
+}
+
+/**
+ * @brief Function for setting the counter mode
+ *
+ * @param NRF_PWMx Pwm instance.
+ *
+ *
+ * @param mode Counter mode.
+ */
+__STATIC_INLINE void nrf_pwm_mode_set(NRF_PWM_Type * NRF_PWMx, nrf_pwm_mode_t mode)
+{
+    NRF_PWMx->MODE = mode;
+}
+
+/**
+ * @brief Function for setting the prescaler
+ *
+ * @param NRF_PWMx Pwm instance.
+ *
+ *
+ * @param mode Prescaler mode.
+ */
+__STATIC_INLINE void nrf_pwm_prescaler_set(NRF_PWM_Type * NRF_PWMx, nrf_pwm_prescaler_t prescaler)
+{
+    NRF_PWMx->PRESCALER = prescaler;
+}
+
+
+/**
+ * @brief Function for setting the COUNTERTOP register
+ *
+ * @param NRF_PWMx Pwm instance.
+ *
+ *
+ * @param countertop COUNTERTOP value.
+ */
+__STATIC_INLINE void nrf_pwm_countertop_set(NRF_PWM_Type * NRF_PWMx, uint32_t countertop)
+{
+    NRF_PWMx->COUNTERTOP = countertop;
+}
+
+
+/**
+ * @brief Function for setting the LOOP register
+ *
+ * @param NRF_PWMx Pwm instance.
+ *
+ *
+ * @param loop LOOP value.
+ */
+__STATIC_INLINE void nrf_pwm_loop_set(NRF_PWM_Type * NRF_PWMx, uint32_t loop)
+{
+    NRF_PWMx->LOOP = loop;
+}
+
+
+/**
+ * @brief Function for setting the DECODER register
+ *
+ * @param NRF_PWMx Pwm instance.
+ *
+ *
+ * @param load LOAD value.
+
+ * @param mode MODE value.
+ */
+__STATIC_INLINE void nrf_pwm_decoder_set(NRF_PWM_Type * NRF_PWMx,
+                                      nrf_pwm_decoder_load_t load,
+                                      nrf_pwm_decoder_mode_t mode)
+{
+    NRF_PWM0->DECODER = (load << PWM_DECODER_LOAD_Pos) |
+        (mode << PWM_DECODER_MODE_Pos);
+
+}
+
+
+
+/**
+ * @brief Function for setting the SEQ register
+ *
+ * @param NRF_PWMx Pwm instance.
+ *
+ *
+ * @param seq Sequence number.
+ *
+ * @param ptr Beginning address in Data RAM
+ *
+ * @param cnt Amount of values (duty cycles)
+ *
+ * @param refresh Amount of additional PWM periods between samples loaded to compare register (load every CNT+1 PWM periods)
+ *
+ * @param enddelay Time added after the sequence in PWM periods
+ *
+ */
+__STATIC_INLINE void nrf_pwm_seq_set(NRF_PWM_Type * NRF_PWMx,
+                                     nrf_pwm_sequence_t seq,
+                                     uint32_t ptr,
+                                     uint32_t cnt,
+                                     uint32_t refresh,
+                                     uint32_t enddelay)
+{
+    NRF_PWMx->SEQ[seq].PTR = ptr;
+    NRF_PWMx->SEQ[seq].CNT = cnt;
+    NRF_PWMx->SEQ[seq].REFRESH = refresh;
+    NRF_PWMx->SEQ[seq].ENDDELAY = enddelay;
+}
